0bb4ef37382bba3db7ddb942cf5644de7678f4ae
lakshaychhabra <lakshay.chhabra@hotmail.com>
2017-07-06 22:23:34 +0530

    Third Commit

--- models/bookSchema.js
+++ models/bookSchema.js
@@ -3,10 +3,16 @@ var Schema = mongoose.Schema;
 
 var book = new Schema({
 
+	book_details:{
 	name : String,
 	author : String,
     bookId:Number,
-    Edition:Number
+    Edition:Number 
+                 },
+    book_status :{
+    dateOfIssue : String,
+	issuedTo : Number
+            }
 });
 var bookInfo = mongoose.model('book',book);
 module.exports = bookInfo;
